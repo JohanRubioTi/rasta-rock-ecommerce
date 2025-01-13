e2336c2472da421089b572781050fda5cbebdb21
johanrubioti <johanrubioti@gmail.com>
2025-01-13 14:19:38 -0500

    Added custom fonts support

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -2,6 +2,11 @@ import { Provider } from 'app/provider'
 import { useFonts } from 'expo-font'
 import { SplashScreen, Stack } from 'expo-router'
 import { useEffect } from 'react'
+import {
+  AtkinsonHyperlegible_400Regular,
+  AtkinsonHyperlegible_700Bold,
+} from '@expo-google-fonts/atkinson-hyperlegible'
+import { SpaceMono_400Regular, SpaceMono_700Bold } from '@expo-google-fonts/space-mono'
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -13,8 +18,12 @@ SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+    DmSans: require('@tamagui/font-dm-sans'),
+    Silkscreen: require('@tamagui/font-silkscreen'),
+    AtkinsonHyperlegible: AtkinsonHyperlegible_400Regular,
+    AtkinsonHyperlegibleBold: AtkinsonHyperlegible_700Bold,
+    SpaceMono: SpaceMono_400Regular,
+    SpaceMonoBold: SpaceMono_700Bold,
   })
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
--- packages/ui/src/tamagui.config.ts
+++ packages/ui/src/tamagui.config.ts
@@ -1,26 +1,28 @@
-import { createInterFont } from '@tamagui/font-inter'
-import { createMedia } from '@tamagui/react-native-media-driver'
-import { shorthands } from '@tamagui/shorthands'
-import { themes, tokens } from '@tamagui/themes'
-import { createTamagui } from 'tamagui'
+import { createDmSansFont } from "@tamagui/font-dm-sans";
+import { createSilkscreenFont } from "@tamagui/font-silkscreen";
+import { createMedia } from "@tamagui/react-native-media-driver";
+import { shorthands } from "@tamagui/shorthands";
+import { themes, tokens } from "@tamagui/themes";
+import { createTamagui } from "tamagui";
+import { createFont } from "@tamagui/core";
 
-import { animations } from './animations'
+import { animations } from "./animations";
 
-const headingFont = createInterFont({
+const headingFont = createDmSansFont({
   size: {
     6: 15,
   },
   transform: {
-    6: 'uppercase',
-    7: 'none',
+    6: "uppercase",
+    7: "none",
   },
   weight: {
-    6: '400',
-    7: '700',
+    6: "400",
+    7: "700",
   },
   color: {
-    6: '$colorFocus',
-    7: '$color',
+    6: "$colorFocus",
+    7: "$color",
   },
   letterSpacing: {
     5: 2,
@@ -34,24 +36,106 @@ const headingFont = createInterFont({
     15: -6,
   },
   face: {
-    700: { normal: 'InterBold' },
+    700: { normal: "DmSansBold" },
   },
-})
+});
 
-const bodyFont = createInterFont(
-  {
-    face: {
-      700: { normal: 'InterBold' },
-    },
+const bodyFont = createFont({
+  family: "Atkinson Hyperlegible",
+
+  size: {
+    6: 15,
   },
-  {
-    sizeSize: (size) => Math.round(size * 1.1),
-    sizeLineHeight: (size) => Math.round(size * 1.1 + (size > 20 ? 10 : 10)),
-  }
-)
+  transform: {
+    6: "uppercase",
+    7: "none",
+  },
+  weight: {
+    6: "400",
+    7: "700",
+  },
+  color: {
+    6: "$colorFocus",
+    7: "$color",
+  },
+  letterSpacing: {
+    5: 2,
+    6: 1,
+    7: 0,
+    8: -1,
+    9: -2,
+    10: -3,
+    12: -4,
+    14: -5,
+    15: -6,
+  },
+});
+
+const monoFont = createFont({
+  family: "Space Mono",
+
+  size: {
+    6: 15,
+  },
+  transform: {
+    6: "uppercase",
+    7: "none",
+  },
+  weight: {
+    6: "400",
+    7: "700",
+  },
+  color: {
+    6: "$colorFocus",
+    7: "$color",
+  },
+  letterSpacing: {
+    5: 2,
+    6: 1,
+    7: 0,
+    8: -1,
+    9: -2,
+    10: -3,
+    12: -4,
+    14: -5,
+    15: -6,
+  },
+});
+
+const accentFont = createSilkscreenFont({
+  size: {
+    6: 15,
+  },
+  transform: {
+    6: "uppercase",
+    7: "none",
+  },
+  weight: {
+    6: "400",
+    7: "700",
+  },
+  color: {
+    6: "$colorFocus",
+    7: "$color",
+  },
+  letterSpacing: {
+    5: 2,
+    6: 1,
+    7: 0,
+    8: -1,
+    9: -2,
+    10: -3,
+    12: -4,
+    14: -5,
+    15: -6,
+  },
+  face: {
+    700: { normal: "SilkscreenBold" },
+  },
+});
 
 export const config = createTamagui({
-  defaultFont: 'body',
+  defaultFont: "body",
   animations,
   shouldAddPrefersColorThemes: true,
   themeClassNameOnRoot: true,
@@ -75,7 +159,7 @@ export const config = createTamagui({
     gtLg: { minWidth: 1280 + 1 },
     short: { maxHeight: 820 },
     tall: { minHeight: 820 },
-    hoverNone: { hover: 'none' },
-    pointerCoarse: { pointer: 'coarse' },
+    hoverNone: { hover: "none" },
+    pointerCoarse: { pointer: "coarse" },
   }),
-})
+});
